5c6626ee14f96560a1d45a41c36b186ad3e54420
mtbishop <bishopmatthew76@gmail.com>
2021-01-05 17:27:26 -0600

    added dynamic question counter with a score

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
+
 
 let currentQuestion = {};
 let acceptingAnswers = true;
@@ -64,6 +67,8 @@ function getNewQuestion() {
     return window.location.assign('/end.html');
   }
   questionCounter++;
+  questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
   question.innerText = currentQuestion.question;
@@ -97,5 +102,9 @@ choices.forEach((choice) => {
     }, 1000);
   });
 });
+
+
+
+
 
 startGame();
