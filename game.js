99e92ed681d678fc79139065d261320b8d740538
mtbishop <bishopmatthew76@gmail.com>
2021-01-05 22:51:48 -0600

    changed css elements and adjusted js

--- game.js
+++ game.js
@@ -3,10 +3,10 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 const questionCounterText = document.getElementById('questionCounter');
 const scoreText = document.getElementById('score');
 
-
 let currentQuestion = {};
 let acceptingAnswers = true;
 let score = 0;
+let penalty = 10;
 let questionCounter = 0;
 let availableQuestions = [];
 
@@ -32,13 +32,15 @@ let questions = [
     choice1: 'Head',
     choice2: 'Both',
     choice3: 'Body',
-    choice4: '???',
+    choice4: 'Footer',
     answer: 2,
   },
   {
-    question: 'Is JavaScript the same as Java?',
-    choice1: 'True',
-    choice2: 'False',
+    question: 'Which event occurs when the user clicks on an HTML element?',
+    choice1: 'onchange',
+    choice2: 'onclick',
+    choice3: 'onmouseover',
+    choice4: 'onmouseclick',
     answer: 2,
   },
   {
@@ -50,30 +52,37 @@ let questions = [
     answer: 2,
   },
 ];
-
+// score value for correct answers
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 4;
+// total amount of questions
+const MAX_QUESTIONS = 5;
 
 function startGame() {
+  // starts at first question
   questionCounter = 0;
   score = 0;
+  // grabs from questions array
   availableQuestions = [...questions];
+  // runs first question
   getNewQuestion();
 }
 
 function getNewQuestion() {
+    // when the last question is answered, it ...
   if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-      localStorage.setItem('mostRecentScore', score);
+    localStorage.setItem('mostRecentScore', score);
     // goes to end page
     return window.location.assign('/end.html');
   }
   questionCounter++;
   questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
 
+  // randomizes question index
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
   question.innerText = currentQuestion.question;
 
+  // selects the correct answer by number value 
   choices.forEach((choice) => {
     const number = choice.dataset['number'];
     choice.innerText = currentQuestion['choice' + number];
@@ -91,28 +100,26 @@ choices.forEach((choice) => {
     acceptingAnswers = false;
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset['number'];
-
+    // adds a class decided by if the question was correct or incorrect
     const classToApply =
       selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
-
-      if(classToApply === "correct") {
-          incrementScore(CORRECT_BONUS);
-      }
+    // adds CORRECT_BONUS value if answer is correct
+    if (classToApply === 'correct') {
+      incrementScore(CORRECT_BONUS);
+    }
 
     selectedChoice.parentElement.classList.add(classToApply);
 
     setTimeout(() => {
       selectedChoice.parentElement.classList.remove(classToApply);
       getNewQuestion();
-    }, 1000);
+    }, 1500);
   });
 });
 
-incrementScore = num => {
-    score += num;
-    scoreText.innerText = score;
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
 };
-
-
 
 startGame();
